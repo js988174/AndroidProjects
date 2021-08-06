b36340b077fe06ea1d768c440e8816ceecb78bd9
HyeonseungSon <boywin1992@naver.com>
2021-08-06 15:55:50 +0900

    2021.08.06 15:54 activity_board, BoardActivity.kt 생성 및 activity_intro.xml 에 버튼 하나 추가

---
+++ app/src/main/java/com/rud/mandeumtalk/board/BoardActivity.kt
@@ -0,0 +1,12 @@
+package com.rud.mandeumtalk.board
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.rud.mandeumtalk.R
+
+class BoardActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_board)
+    }
+}
